6aa61fb3ce904123d1b25bbec4cfd772903543a9
Dean Mark <deansy0718@gmail.com>
2025-07-02 15:35:19 +0800

    done index.html, js, css. Proceeding to cart page

--- js/main.js
+++ js/main.js
@@ -5,11 +5,17 @@ for (let i = 0; i < products.length; i++) {
 
     const card = document.createElement('div');
     card.className = 'product-cart';
-    card.innerHTML = `<img src="${getProducts.img}" alt="${getProducts.name}">
+    card.innerHTML = `<img class="product-img" src="${getProducts.img}" alt="${getProducts.name}">
                         <h3>${getProducts.name}</h3>
-                        <p>${getProducts.price}</p>
-                        <button onclick="${getProducts.id}">Add to Cart</button>
+                        <p>₱${getProducts.price}</p>
+                        <button onclick="addCart(${getProducts.id})">Add to Cart</button>
     `;
 
     container.appendChild(card);
 }
+function addCart(id) {
+let cart = [];
+const getId = products.find((p) => p.id === id);
+cart.push(getId);
+alert('Added to cart!')
+};
--- js/product.js
+++ js/product.js
@@ -3,34 +3,69 @@ const products = [
     id: 1,
     name: 'Redmi 9T',
     price: 7999,
-    img: "https://computermice.wordpress.com/wp-content/uploads/2014/11/3-tastenmaus_microsoft.jpg"
+    img: "https://philregalo.com/cdn/shop/products/Xiaomi-Redmi-9T_1.jpg?v=1614659452"
     },
 
     {
     id: 2,
     name: 'Mouse',
     price: 499,
-    img: 'images/mouse.jpg'
+    img: 'https://computermice.wordpress.com/wp-content/uploads/2014/11/3-tastenmaus_microsoft.jpg'
     },
 
     {
     id: 3,
     name: 'Charger',
     price: 689,
-    img: 'images/charger.jpg'
+    img: 'https://anker.ph/cdn/shop/files/B2697141-01_1800x.png?v=1737966450'
     },
 
     {
     id: 4,
     name: 'Keyboard',
     price: 1499,
-    img: 'images/keyboard.jpg'
+    img: 'https://digitalwalker.ph/cdn/shop/files/SM1MechanicalKeyboard_Light_-USLightGray1.png?v=1721119717'
     },
 
     {
     id: 5,
     name: 'Headset',
     price: 1299,
-    img: 'images/Headset.jpg'
-    }
+    img: 'https://i5.walmartimages.com/asr/925f83fe-e929-438a-b118-5f23a5a90225.8c142351a49f466f29f6871d100871c4.jpeg'
+    },
+
+    {
+    id: 6,
+    name: 'Airpods pro 2',
+    price: 4899,
+    img: "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/airpods-pro-2-hero-select-202409_FMT_WHH?wid=750&hei=556&fmt=jpeg&qlt=90&.v=1724041668836"
+    },
+
+    {
+    id: 7,
+    name: 'Rog Strix Bag',
+    price: 4588,
+    img: "https://dlcdnwebimgs.asus.com/files/media/7307645F-321A-46F8-8DED-BEA6BFC2A968/v1/img/bundle/pd/1x/ROG-backpack-4.png"
+    },
+
+    {
+    id: 8,
+    name: 'Thinkpad E18',
+    price: 27999,
+    img: "https://content.etilize.com/900/1077937526.jpg?noimage=logo"
+    },
+
+    {
+    id: 9,
+    name: 'Holder',
+    price: 199,
+    img: "https://www.truevision.com.ph/wp-content/uploads/2023/10/TVPH16-4.jpg"
+    },
+
+    {
+    id: 10,
+    name: 'i9 14th 14900k',
+    price: 29599,
+    img: "https://ecommerce.datablitz.com.ph/cdn/shop/files/19-118-462-01_1024x.jpg?v=1697684589"
+    },
 ];
